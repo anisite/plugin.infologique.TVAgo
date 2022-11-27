2793745c6391cdf748f5886a0c5ade89b29ab3c5
Math <tatou100@gmail.com>
2022-11-26 22:08:52 -0500

    revert clearcache modification.

--- resources/lib/clearcache.py
+++ resources/lib/clearcache.py
@@ -9,11 +9,11 @@ import xbmcvfs
 from xbmcaddon import Addon
 
 addon = Addon('plugin.infologique.TVAgo')
-addon_cache_basedir = os.path.join(xbmc.translatePath(addon.getAddonInfo('path')),".cache")
+addon_cache_basedir = os.path.join(xbmc.translatePath(addon.getAddonInfo('path')).decode('utf-8'),".cache")
 
 if sys.argv[1].lower() == "full":
-    print("["+addon.getAddonInfo('name')+"] deleting full cache")
+    print "["+addon.getAddonInfo('name')+"] deleting full cache"
     for root, dirs, files in os.walk(addon_cache_basedir):
         for file in files:
             xbmcvfs.delete(os.path.join(root,file))
-
+    xbmcgui.Dialog().ok(addon.getAddonInfo('name'), addon.getLocalizedString(32108),addon.getLocalizedString(32109))
